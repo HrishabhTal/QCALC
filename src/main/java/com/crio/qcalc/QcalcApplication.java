85b82a035e6d542a83e55367827107f3eefeff80
Shinichi <rishabhatre@gmail.com>
2023-08-03 17:10:59 +0530

    first commit

---
+++ src/main/java/com/crio/qcalc/QcalcApplication.java
@@ -0,0 +1,19 @@
+package com.crio.qcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcalcApplication.class, args);
+		StandardCalculator calc = new StandardCalculator();
+		calc.add(1, 1);
+		System.out.println(calc.getResult());
+
+
+		
+	}
+
+}
